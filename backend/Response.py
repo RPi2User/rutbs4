6bf926b3c880ec8f5e45d382a6333dbb70299852
RPi2User <florian@hatzfeld.biz>
2025-09-06 12:58:21 +0200

    Response.py: Removed Debug stuff

--- backend/Response.py
+++ backend/Response.py
@@ -2,13 +2,6 @@ from flask.wrappers import Response as BaseResponse
 import json
 
 class Response(BaseResponse):
-
-    """
-    def __init__(self, body: str, mimetype: str, status_code: int):
-        self = BaseResponse(response=body, status=status_code, mimetype=mimetype)
-        self.body = self.response
-        self.status_code = self.status
-    """
     
     def _asdict(self) -> dict:
         data = {
@@ -16,8 +9,6 @@ class Response(BaseResponse):
             "mimetype" : self.mimetype,
             "status_code": self.status
         }
-        print("RESPONSE DEBUG: -----------------------------")
-        print(data)
         return data
 
     def __str__(self) -> str:
