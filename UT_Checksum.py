b987fd14847e8a00177d2238637d8c4a2115bd65
RPi2User <florian@hatzfeld.biz>
2026-01-08 19:09:52 +0100

    UT_Checksum: Re-wrote checksum unit tests properly

--- UT_Checksum.py
+++ UT_Checksum.py
@@ -2,107 +2,206 @@ import unittest
 
 # Module imports
 from backend.Checksum import Checksum, ChecksumState, ChecksumType
-from backend.File import File
+from backend.File import File, FileState
 
 class UT_Checksum(unittest.TestCase):
 
-    MD5: str = "07f9b8f14c23725624cb0745e24e1f98" # old "034665aa051c1fe748ad8982347cb320"
-    SHA256: str = "41057efcb2dd05930ca173d1fcd68ad43c60d5936527e01f8f43c62e184072a0" # old "42c497d0b2e97b031c172eb2cbb3e389b7c2355b404a1b7afeefef99cec06baf"
+    MD5: str            = "07f9b8f14c23725624cb0745e24e1f98"
+    MD5_SPOILED: str    = "07f9b8f14c23725624cb0745e24e1___"
+    SHA256: str         = "41057efcb2dd05930ca173d1fcd68ad43c60d5936527e01f8f43c62e184072a0"
+    SHA256_SPOILED: str = "41057efcb2dd05930ca173d1fcd68ad43c60d5936527e01f8f43c62e18407___"
+    SHA512: str         = "bfbce2b251d7c0718f69dfc9693999c5095d23a8e4fed0370673031c563a46deafe02822e22631edcf15fcbbb8861989ed95e3e73d112e3a7f63d828b8aba3f9"
+    SHA512_SPOILED: str = "bfbce2b251d7c0718f69dfc9693999c5095d23a8e4fed0370673031c563a46deafe02822e22631edcf15fcbbb8861989ed95e3e73d112e3a7f63d828b8aba___"
+    
+    PATH: str = "/mnt/daten/testfiles/rutbs4/cksum/test100.raw"
+    CONTEXT: str = "/mnt/daten/testfiles/rutbs4"
+    SIZE: int = 104857600
 
-    def test_create_md5(self):
-        file: File = File(1, "./testing/test_1/test100.raw")
-        c: Checksum = Checksum(file.path, ChecksumType.MD5)
+    def test_A_create_md5(self):
+        file: File = File(1, self.PATH, self.CONTEXT)
+        file.cksum.setType(ChecksumType.MD5)
 
-        file.setChecksum(c)
+        try:
+            self.assertEqual(len(file.cksum.value), 0)
+
+            file.createChecksum()
+
+            self.assertEqual(file.state, FileState.CKSUM_CALC)
+            self.assertEqual(len(file.cksum.value), 0)
+            self.assertEqual(file.cksum.cmd.filesize, self.SIZE)
+
+            file.wait()
+
+            self.assertEqual(file.state, FileState.IDLE)
+            self.assertEqual(file.cksum.state, ChecksumState.IDLE)
+            self.assertEqual(file.cksum.value, self.MD5)
+        except AssertionError:
+            print(file)
+            raise
+
+        print(".A_CREATE_MD5")
+
+    def test_B_create_sha256(self):
+        file: File = File(1, self.PATH, self.CONTEXT)
         file.createChecksum()
 
-        file.cksum.wait()
-        file._asdict()
+        file.wait()
 
-        #print(json.dumps(file._asdict(), indent=2))    # this triggers a refresh of all params
+        try:
+            self.assertEqual(file.cksum.state, ChecksumState.IDLE)
+            self.assertEqual(file.cksum.value, self.SHA256)
+        except AssertionError:
+            print(file)
+            raise
 
-        self.assertEqual(file.cksum.state, ChecksumState.IDLE)
-        self.assertEqual(file.cksum.value, self.MD5)
+        print("B_CREATE_SHA256")
 
-
-    def test_create_sha256(self):
-        file: File = File(1, "./testing/test_1/test100.raw")
-        c: Checksum = Checksum(file.path, ChecksumType.SHA256)
-
-        file.setChecksum(c)
+    def test_C_create_sha512(self):
+        file: File = File(1, self.PATH, self.CONTEXT)
+        file.cksum.setType(ChecksumType.SHA512)
         file.createChecksum()
 
-        file.cksum.wait()
-        file._asdict()
+        file.wait()
 
-        self.assertEqual(file.cksum.state, ChecksumState.IDLE)
-        self.assertEqual(file.cksum.value, self.SHA256)
+        try:
+            self.assertEqual(file.cksum.state, ChecksumState.IDLE)
+            self.assertEqual(file.cksum.value, self.SHA512)
+        except AssertionError:
+            print(file)
+            raise
 
+        print("C_CREATE_SHA512")
 
-    def test_check_md5(self) -> None:
-        file: File = File(1, "./testing/test_1/test100.raw")
-        c: Checksum = Checksum(file_path=file.path, value=self.MD5)
-        self.assertEqual(c.type, ChecksumType.SHA256)
+    def test_D_validate_md5(self) -> None:
+        file: File = File(1, self.PATH, self.CONTEXT)
+        file.cksum.setType(ChecksumType.MD5)
 
-        c.setType(ChecksumType.MD5) # This get Called to check the if changing cktype is possible
-        
-        file.setChecksum(c)
-        file.validateIntegrity()
+        file.validateIntegrity(self.MD5)
 
-        file.cksum.wait()
-        file._asdict()
+        try:
+            self.assertEqual(file.state, FileState.VALIDATING)
+            self.assertEqual(file.cksum.state, ChecksumState.VALIDATE)
 
-        self.assertEqual(file.cksum.validation_target, self.MD5)
-        self.assertEqual(file.cksum.state, ChecksumState.IDLE)
-        self.assertEqual(file.cksum.value, self.MD5)
+            file.wait()
 
-    def test_fail_md5(self) -> None:
-        file: File = File(1, "./testing/test_1/test100.raw")
-        c: Checksum = Checksum(file.path, ChecksumType.MD5)
-        wrong_sum = self.MD5[:-1] + "q"   # add a invalid hex-char
-        c.value = wrong_sum
-        
-        file.setChecksum(c)
-        file.validateIntegrity()
+            self.assertEqual(file.state, FileState.IDLE)
+            self.assertEqual(file.cksum.state, ChecksumState.IDLE)
 
-        file.cksum.wait()
-        file._asdict()
+        except AssertionError:
+            print(file)
+            raise
 
-        self.assertEqual(file.cksum.validation_target, wrong_sum)
-        self.assertEqual(file.cksum.state, ChecksumState.MISMATCH)
-        self.assertEqual(file.cksum.value, self.MD5)
+        print("D_VAL_MD5")
 
-    def test_check_sha256(self) -> None:
-        file: File = File(1, "./testing/test_1/test100.raw")
-        c: Checksum = Checksum(file.path, ChecksumType.SHA256)
-        c.value = self.SHA256
-        
-        file.setChecksum(c)
-        file.validateIntegrity()
+    def test_E_validate_sha256(self) -> None:
+        file: File = File(1, self.PATH, self.CONTEXT)
 
-        file.cksum.wait()
-        file._asdict()
+        file.validateIntegrity(self.SHA256)
 
-        self.assertEqual(file.cksum.validation_target, self.SHA256)
-        self.assertEqual(file.cksum.state, ChecksumState.IDLE)
-        self.assertEqual(file.cksum.value, self.SHA256)
+        try:
+            self.assertEqual(file.state, FileState.VALIDATING)
+            self.assertEqual(file.cksum.state, ChecksumState.VALIDATE)
 
+            file.wait()
 
-    def test_fail_sha256(self) -> None:
-        file: File = File(1, "./testing/test_1/test100.raw")
-        c: Checksum = Checksum(file.path, ChecksumType.SHA256)
-        wrong_sum = self.SHA256[:-1] + "q"   # add a invalid hex-char
-        c.value = wrong_sum
-        
-        file.setChecksum(c)
-        file.validateIntegrity()
+            self.assertEqual(file.state, FileState.IDLE)
+            self.assertEqual(file.cksum.state, ChecksumState.IDLE)
 
-        file.cksum.wait()
-        file._asdict()
+        except AssertionError:
+            print(file)
+            raise
 
-        self.assertEqual(file.cksum.validation_target, wrong_sum)
-        self.assertEqual(file.cksum.state, ChecksumState.MISMATCH)
-        self.assertEqual(file.cksum.value, self.SHA256)
+        print("E_VAL_SHA256")
+
+    def test_F_validate_sha512(self) -> None:
+        file: File = File(1, self.PATH, self.CONTEXT)
+        file.cksum.setType(ChecksumType.SHA512)
+
+        file.validateIntegrity(self.SHA512)
+
+        try:
+            self.assertEqual(file.state, FileState.VALIDATING)
+            self.assertEqual(file.cksum.state, ChecksumState.VALIDATE)
+
+            file.wait()
+
+            self.assertEqual(file.state, FileState.IDLE)
+            self.assertEqual(file.cksum.state, ChecksumState.IDLE)
+
+        except AssertionError:
+            print(file)
+            raise
+
+        print("F_VAL_SHA512")
+
+    def test_G_valSpoil_md5(self) -> None:
+        file: File = File(1, self.PATH, self.CONTEXT)
+        file.cksum.setType(ChecksumType.MD5)
+
+        file.validateIntegrity(self.MD5_SPOILED)
+
+        try:
+            self.assertEqual(file.state, FileState.VALIDATING)
+            self.assertEqual(file.cksum.state, ChecksumState.VALIDATE)
+
+            file.wait()
+
+            self.assertEqual(file.state, FileState.MISMATCH)
+            self.assertEqual(file.cksum.state, ChecksumState.MISMATCH)
+            self.assertEqual(file.state_msg[0], "[ERROR] Checksum mismatch!")
+            self.assertNotEqual(file.cksum.value, file.cksum.validation_target)
+
+        except AssertionError:
+            print(file)
+            raise
+
+        print("G_SPOIL_MD5")
+
+    def test_H_valSpoil_sha256(self) -> None:
+        file: File = File(1, self.PATH, self.CONTEXT)
+
+        file.validateIntegrity(self.SHA256_SPOILED)
+
+        try:
+            self.assertEqual(file.state, FileState.VALIDATING)
+            self.assertEqual(file.cksum.state, ChecksumState.VALIDATE)
+
+            file.wait()
+
+            self.assertEqual(file.state, FileState.MISMATCH)
+            self.assertEqual(file.cksum.state, ChecksumState.MISMATCH)
+            self.assertEqual(file.state_msg[0], "[ERROR] Checksum mismatch!")
+            self.assertNotEqual(file.cksum.value, file.cksum.validation_target)
+
+        except AssertionError:
+            print(file)
+            raise
+
+        print("H_SPOIL_SHA256")
+
+    def test_I_valSpoil_sha512(self) -> None:
+        file: File = File(1, self.PATH, self.CONTEXT)
+        file.cksum.setType(ChecksumType.SHA512)
+
+        file.validateIntegrity(self.SHA512_SPOILED)
+
+        try:
+            self.assertEqual(file.state, FileState.VALIDATING)
+            self.assertEqual(file.cksum.state, ChecksumState.VALIDATE)
+
+            file.wait()
+
+            self.assertEqual(file.state, FileState.MISMATCH)
+            self.assertEqual(file.cksum.state, ChecksumState.IDLE)
+            self.assertEqual(file.state_msg[0], "[ERROR] Checksum mismatch!")
+            self.assertNotEqual(file.cksum.value, file.cksum.validation_target)
+
+        except AssertionError:
+            print(file)
+            raise
+
+        print("I_SPOIL_SHA512")
+
 
 if __name__ == '__main__':
     unittest.main()
