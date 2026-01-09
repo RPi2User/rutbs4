570860763b768418a502b029b25479934980f54e
RPi2User <florian@hatzfeld.biz>
2026-01-09 15:44:11 +0100

    UT_Command: Added Test scenarios for all weird and wonderful hex values

--- UT_Command.py
+++ UT_Command.py
@@ -21,6 +21,8 @@ class UT_Command(unittest.TestCase):
     AL_HEXOUT: str = "dd if=/dev/urandom bs=512 count=1 2>/dev/null"
     AN_CLEANUP: str = "echo 0"
     AN_CLEANUP1: str = "echo the quick brown fox jumps over the lazy dog"
+    AO_RAW_NL: str = "cat /mnt/daten/testfiles/rutbs4/cmd/00_0f.txt" 
+    AP_RAW_EXTREME: str = "cat /mnt/daten/testfiles/rutbs4/cmd/00_ff.txt"
 
     """_summary_
     Depdencies:
@@ -28,6 +30,7 @@ class UT_Command(unittest.TestCase):
     - pwd
     - ping
     - dd
+    - printf
     """
 
     def test_AA_create(self) -> None:
@@ -399,6 +402,55 @@ class UT_Command(unittest.TestCase):
             raise
 
         print("N")
+
+    def test_AO_rawWithNewLines(self) -> None:
+        c: Command = Command(self.AO_RAW_NL, raw=True)
+
+        c.wait()
+
+        try:
+            self.assertEqual(len(c.stdout), 1)
+            self.assertEqual(len(c.stderr), 0)
+            self.assertEqual(c.exitCode, 0)
+            self.assertEqual(c.stdout[0], "000102030405060708090a0b0c0d0e0f")
+        except:
+            c.quiet = False
+            print(c)
+            raise
+        print("O_RAW_NL")
+
+    def test_AP_rawExtreme(self) -> None:
+        c: Command = Command(self.AP_RAW_EXTREME, raw=True)
+
+        c.wait()
+
+        try:
+            self.assertEqual(len(c.stdout), 1)
+            self.assertEqual(len(c.stderr), 0)
+            self.assertEqual(c.exitCode, 0)
+            self.assertEqual(c.stdout[0], "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9fa0a1a2a3a4a5a6a7a8a9aaabacadaeafb0b1b2b3b4b5b6b7b8b9babbbcbdbebfc0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedfe0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff")
+        except:
+            c.quiet = False
+            print(c)
+            raise
+        print("P_RAW_ALL")
+
+    def test_AQ_rawAllFail(self) -> None:
+        c: Command = Command(self.AP_RAW_EXTREME, raw=False)
+
+        c.wait()
+
+        try:
+            self.assertEqual(len(c.stdout), 1)
+            self.assertEqual(len(c.stderr), 0)
+            self.assertEqual(c.exitCode, 0)
+            self.assertEqual(c.stdout[0], "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9fa0a1a2a3a4a5a6a7a8a9aaabacadaeafb0b1b2b3b4b5b6b7b8b9babbbcbdbebfc0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedfe0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff")
+        except:
+            c.quiet = False
+            print(c)
+            raise
+        print("Q_RAW_ALLFAIL")
+
 
 if __name__ == '__main__':
     unittest.main()
