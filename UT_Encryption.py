4d3c5b2b232ec58e3ad4dc00e7015c376deacda3
RPi2User <florian@hatzfeld.biz>
2026-01-12 16:57:59 +0100

    UT_Encryption: Finished Unit tests (successfully)

--- UT_Encryption.py
+++ UT_Encryption.py
@@ -3,19 +3,14 @@ import json
 
 # Module imports
 from backend.Encryption import *
-from backend.File import File
+from backend.File import File, FileState
 from backend.Checksum import *
 
 class UT_Encryption(unittest.TestCase):
 
-    SHA256: str =   "e4104dda1a72365ab706bb2500dc49534465d3b9e597a16ddc38160e5dda8ea0"
+    SHA256: str =   "41057efcb2dd05930ca173d1fcd68ad43c60d5936527e01f8f43c62e184072a0"
     PATH: str =     "/mnt/daten/testfiles/rutbs4/encryption/test100.raw"
     CONTEXT: str =  "/mnt/daten/testfiles/rutbs4"
-
-    AES128CBC: str = "PUT SHA256 HERE"
-    AES256CBC: str = "PUT SHA256 HERE"
-    AES128CTR: str = "PUT SHA256 HERE"
-    AES256CTR: str = "PUT SHA256 HERE"
 
     KEY_SHORT: str =    "7b3cb4de93854612e9b376bad4911832"
     KEY_MEDIUM: str =   "9b406f953ba4591ce394f6a88b4bf365cf521b27db932a8fd07231a2a2b2b9be"
@@ -26,7 +21,10 @@ class UT_Encryption(unittest.TestCase):
     def test_AA_keygen_short(self):
         k: Key = Key(KeyLength.short)
         try:
+            self.assertEqual(k.length.name, "short")
             self.assertEqual(len(k.value), k.length.value * 2)
+            self.assertEqual(len(k.iv), 32)
+            self.assertNotEqual(k.value, k.iv)
         except AssertionError:
             print(k)
             raise
@@ -35,7 +33,10 @@ class UT_Encryption(unittest.TestCase):
     def test_AB_keygen_medium(self):
         k: Key = Key(KeyLength.medium)
         try:
+            self.assertEqual(k.length.name, "medium")
             self.assertTrue(len(k.value), k.length.value * 2)
+            self.assertEqual(len(k.iv), 32)
+            self.assertNotEqual(k.value, k.iv)
         except AssertionError:
             print(k)
             raise
@@ -45,107 +46,241 @@ class UT_Encryption(unittest.TestCase):
 
     def test_BA_enc_aes128cbc(self) -> None:
         f: File = File(1, self.PATH, self.CONTEXT)
+        k: Key = Key(KeyLength.short)
+        k.value = self.KEY_SHORT
+        k.iv = self.IV
 
         try:
-            self.assertIsInstance(f.encryption_scheme, File)
+            e: Encryption = Encryption(k, E_Mode.AES128CBC)
+            e.keepOrig = False
+            e.cmd.filesize = f.size
+            f.encryption_scheme = e
+
+            self.assertEqual(f.encryption_scheme, e)
+            self.assertEqual(f.state, FileState.IDLE)
+            self.assertEqual(f.encryption_scheme.state, E_State.IDLE)
+
+            # 1. Encrypt File
+            f.encrypt()
+            self.assertEqual(f.state, FileState.ENCRYPT)
+            self.assertEqual(f.encryption_scheme.state, E_State.ENCRYPT)
+            self.assertEqual(f.encryption_scheme.targetPath, self.PATH + ".crypt")
+            f.wait()
+
+            # 2. Check if Checksum indeed invalid
+            f.validateIntegrity(self.SHA256)
+            f.wait()
+
+            self.assertEqual(len(f.state_msg), 1)
+            self.assertEqual(f.state, FileState.MISMATCH)
+            self.assertEqual(f.state_msg[0], "[ERROR] Checksum mismatch!")
+            self.assertEqual(f.encryption_scheme.state, E_State.IDLE)
+
+            # 3. Decrypt File
+            f.state = FileState.IDLE    # state machine will block further operations
+            f.decrypt()
+            self.assertEqual(f.state, FileState.DECRYPT)
+            self.assertEqual(f.encryption_scheme.state, E_State.DECRYPT)
+            self.assertEqual(f.encryption_scheme.targetPath, self.PATH)
+            f.wait()
+
+            self.assertEqual(f.state, FileState.IDLE)
+            self.assertEqual(f.encryption_scheme.state, E_State.IDLE)
+            self.assertEqual(len(f.state_msg), 1)
+
+            # 4. Check if we restored the correct file :3
+            f.validateIntegrity(self.SHA256)
+            f.wait()
+
+            self.assertEqual(len(f.state_msg), 1)
+            self.assertEqual(f.state, FileState.IDLE)
+            self.assertEqual(f.encryption_scheme.state, E_State.IDLE)
+
         except AssertionError:
             print(f)
             raise
 
-        print("BA_ENC_AES128CBC")
+        print("BA_AES128CBC")
 
-    def test_BB_enc_aes128ctr(self) -> None:
+    def test_BB_aes128ctr(self) -> None:
         f: File = File(1, self.PATH, self.CONTEXT)
+        k: Key = Key(KeyLength.short)
+        k.value = self.KEY_SHORT
+        k.iv = self.IV
 
         try:
-            self.assertIsInstance(f.encryption_scheme, Encryption)
+            e: Encryption = Encryption(k, E_Mode.AES128CTR)
+            e.keepOrig = False
+            e.cmd.filesize = f.size
+            f.encryption_scheme = e
+
+            # 1. Encrypt File
+            f.encrypt()
+            f.wait()
+
+            # 2. Check if Checksum indeed invalid
+            f.validateIntegrity(self.SHA256)
+            f.wait()
+
+            self.assertEqual(len(f.state_msg), 1)
+            self.assertEqual(f.state, FileState.MISMATCH)
+            self.assertEqual(f.state_msg[0], "[ERROR] Checksum mismatch!")
+            self.assertEqual(f.encryption_scheme.state, E_State.IDLE)
+
+            # 3. Decrypt File
+            f.state = FileState.IDLE    # state machine will block further operations
+            f.decrypt()
+            f.wait()
+
+            # 4. Check if we restored the correct file :3
+            f.validateIntegrity(self.SHA256)
+            f.wait()
+
+            self.assertEqual(len(f.state_msg), 1)
+            self.assertEqual(f.state, FileState.IDLE)
+            self.assertEqual(f.encryption_scheme.state, E_State.IDLE)
+
         except AssertionError:
             print(f)
             raise
 
-        print("BB_ENC_AES128CTR")
+        print("BB_AES128CTR")
 
-    def test_BC_enc_aes256cbc(self) -> None:
+    def test_BC_aes256cbc(self) -> None:
         f: File = File(1, self.PATH, self.CONTEXT)
+        k: Key = Key(KeyLength.short)
+        k.value = self.KEY_SHORT
+        k.iv = self.IV
 
         try:
-            self.assertIsInstance(f.encryption_scheme, Encryption)
+            e: Encryption = Encryption(k, E_Mode.AES256CBC)
+            e.keepOrig = False
+            e.cmd.filesize = f.size
+            f.encryption_scheme = e
+
+            # 1. Encrypt File
+            f.encrypt()
+            f.wait()
+
+            # 2. Check if Checksum indeed invalid
+            f.validateIntegrity(self.SHA256)
+            f.wait()
+
+            self.assertEqual(len(f.state_msg), 1)
+            self.assertEqual(f.state, FileState.MISMATCH)
+            self.assertEqual(f.state_msg[0], "[ERROR] Checksum mismatch!")
+            self.assertEqual(f.encryption_scheme.state, E_State.IDLE)
+
+            # 3. Decrypt File
+            f.state = FileState.IDLE    # state machine will block further operations
+            f.decrypt()
+            f.wait()
+
+            # 4. Check if we restored the correct file :3
+            f.validateIntegrity(self.SHA256)
+            f.wait()
+
+            self.assertEqual(len(f.state_msg), 1)
+            self.assertEqual(f.state, FileState.IDLE)
+            self.assertEqual(f.encryption_scheme.state, E_State.IDLE)
+
         except AssertionError:
             print(f)
             raise
 
-        print("BC_ENC_AES256CBC")
+        print("BC_AES256CBC")
 
-    def test_BD_enc_aes256ctr(self) -> None:
+    def test_BD_aes256ctr(self) -> None:
         f: File = File(1, self.PATH, self.CONTEXT)
+        k: Key = Key(KeyLength.short)
+        k.value = self.KEY_SHORT
+        k.iv = self.IV
 
         try:
-            self.assertIsInstance(f.encryption_scheme, Encryption)
+            e: Encryption = Encryption(k, E_Mode.AES256CTR)
+            e.keepOrig = False
+            e.cmd.filesize = f.size
+            f.encryption_scheme = e
+
+            # 1. Encrypt File
+            f.encrypt()
+            f.wait()
+
+            # 2. Check if Checksum indeed invalid
+            f.validateIntegrity(self.SHA256)
+            f.wait()
+
+            self.assertEqual(len(f.state_msg), 1)
+            self.assertEqual(f.state, FileState.MISMATCH)
+            self.assertEqual(f.state_msg[0], "[ERROR] Checksum mismatch!")
+            self.assertEqual(f.encryption_scheme.state, E_State.IDLE)
+
+            # 3. Decrypt File
+            f.state = FileState.IDLE    # state machine will block further operations
+            f.decrypt()
+            f.wait()
+
+            # 4. Check if we restored the correct file :3
+            f.validateIntegrity(self.SHA256)
+            f.wait()
+
+            self.assertEqual(len(f.state_msg), 1)
+            self.assertEqual(f.state, FileState.IDLE)
+            self.assertEqual(f.encryption_scheme.state, E_State.IDLE)
+
         except AssertionError:
             print(f)
             raise
 
-        print("BD_ENC_AES256CTR")
+        print("BD_AES256CTR")
 
-"""
+    def test_CA_ALL_MEDIUM(self) -> None:
+        f: File = File(1, self.PATH, self.CONTEXT)
+        k: Key = Key(KeyLength.medium)
+        k.value = self.KEY_MEDIUM
+        k.iv = self.IV
+        result: dict = {}
 
-    def test_default(self):
-        k: Key = Key() # create key
-        e: Encryption = Encryption(k)
-        path: str = "./testing/testfiles/test100.raw"
-        path_enc: str = "./testing/testfiles/test100.raw.crypt"
+        for scheme in E_Mode:
 
-        # --- File Init + Checksum
-        file: File = File(1, path)
-        file = self._checkChecksum(file, self.SHA256)
-        # ----------------------------------------------------------
+            try:
+                e: Encryption = Encryption(k, scheme)
+                e.keepOrig = False
+                e.cmd.filesize = f.size
+                f.encryption_scheme = e
 
+                # 1. Encrypt File
+                f.encrypt()
+                f.wait()
 
-        # --- Encrypt target and re-check Checksum
-        file.encrypt(e)
+                # 2. Check if Checksum indeed invalid
+                f.createChecksum()
+                f.wait()
 
-        while file.encryption_scheme.state == E_State.ENCRYPT:
-            file._asdict()  # poll status as long as the encryption runs
+                self.assertNotEqual(f.cksum.value, self.SHA256)
+                self.assertEqual(f.encryption_scheme.state, E_State.IDLE)
 
-        file.cksum.file_path = file.path     # refresh path var to path + ".crypt"
-        self._checkChecksum(file, self.SHA256, True)
-        file.path = "./testing/testfiles/test100.raw"
-        file.remove()
-        # ----------------------------------------------------------
+                # 3. Decrypt File
+                f.state = FileState.IDLE    # state machine will block further operations
+                f.decrypt()
+                f.wait()
 
-        # --- Decrypt File
-        file: File = File(1, path_enc)
-        file.decrypt(e)
-        file.encryption_scheme.cmd.wait()
+                # 4. Check if we restored the correct file :3
+                f.validateIntegrity(self.SHA256)
+                f.wait()
 
-        while file.encryption_scheme.state == E_State.DECRYPT:
-            file._asdict()  # poll status as long as the decryption runs
+                self.assertEqual(len(f.state_msg), 0)
+                self.assertEqual(f.state, FileState.IDLE)
+                self.assertEqual(f.encryption_scheme.state, E_State.IDLE)
 
-        file.cksum.file_path = file.path    #TODO migrate this into file.encrypt() / file.decrypt()
-        self._checkChecksum(file, self.SHA256)
+            except AssertionError:
+                print(f)
+                raise
 
-        print(file)
-        
+            result.update({scheme.name: f._asdict()})
+        #print(json.dumps(result, indent=2))
+        print("CA_ALL_MEDIUM")
 
-
-
-    def _checkChecksum(self, file: File, value: str, inverse: bool = False) -> File:
-        c: Checksum = Checksum(file.path, value=value)
-        file.setChecksum(c)
-        file.validateIntegrity()
-
-        file.cksum.wait()
-
-        self.assertEqual(file.cksum.validation_target, value)
-        if inverse:
-            self.assertEqual(file.cksum.state, ChecksumState.MISMATCH)
-            self.assertNotEqual(file.cksum.value, value)
-        else:
-            self.assertEqual(file.cksum.state, ChecksumState.IDLE)
-            self.assertEqual(file.cksum.value, value)
-        return file
-
-"""
 
 if __name__ == '__main__':
     unittest.main()
