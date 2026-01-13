c02f15a551addfeb902f84fd3d4b9127a83f9765
RPi2User <florian@hatzfeld.biz>
2026-01-13 19:44:28 +0100

    UnitTest.sh: Just some eye candy

--- UnitTest.sh
+++ UnitTest.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 # Just a quick caller for batch-testing
 
+echo Command
 python3 UT_Command.py
+
+echo File
 python3 UT_File.py
+
+echo Checksum
 python3 UT_Checksum.py
+
+echo Encryption
 python3 UT_Encryption.py
+
+echo Tape
+python3 UT_Tape.py
